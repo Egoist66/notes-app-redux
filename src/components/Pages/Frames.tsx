28aa40142587874740946045ca3b22c83848839c
Farid Mahmudov <razormad666@gmail.com>
2023-11-09 03:39:13 +0300

    fix16

--- src/components/Pages/Frames.tsx
+++ src/components/Pages/Frames.tsx
@@ -4,6 +4,7 @@ import {FrameForm} from "../FrameForm";
 import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
 import {FramesView} from "../FramesView";
 import {getFramesFromLS} from "../../redux/todo-frames-slice";
+import {Typography} from "antd";
 
 
 const StyledFrames = styled.div`
@@ -25,6 +26,7 @@ const Frames: FC = () => {
 
     const {frames} = useAppSelector(state => state.todoFrames)
     const dispatch = useAppDispatch()
+    const {Text} = Typography
 
     const frameElements = frames.map(frame => (
         <FramesView id={frame.id} key={frame.id} url={frame.url} />
@@ -37,6 +39,15 @@ const Frames: FC = () => {
 
     return (
         <>
+            <Text style={{paddingBottom: 50, display: 'block'}} underline type={'secondary'}>
+                При использовании ресурсов из платформы YouTube необходимо в корректном формате
+                импортировать фрейм.<br/> Для этого выберете подходящее видео далее нажмите на
+                "Поделиться" ➡️ "Встроить" ➡️ "Скопировать HTML" и вставить в поле ввода приложения.<br/>
+                <Text type={'danger'}>Опция фреймов работает полностью только на ОС компьютеров и Планшетов.
+                    На мобильных устройствах функция "Во весь экран" не работает"
+                </Text>
+            </Text>
+
             <FrameForm />
 
             <StyledFrames id={'frames'}>
